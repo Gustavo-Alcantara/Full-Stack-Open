86a006a4d63ba57c74b90ebc052ddc29a8db7eae
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2023-01-02 23:03:15 -0300

    course information step3 done

--- Part 1/courseinformation/src/App.js
+++ Part 1/courseinformation/src/App.js
@@ -1,17 +1,23 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={[{name:part1, exercises:exercises1},{name:part2, exercises:exercises2},{name:part3, exercises:exercises3}]}/>
-      <Total num={exercises1+exercises2+exercises3}/>
+      <Content parts={[part1,part2,part3]}/>
+      <Total parts={[part1,part2,part3]}/>
     </div>
   )
 }
@@ -30,7 +36,11 @@ const Content = ({parts}) =>{
 const Part = ({part}) =>{
   return(<p>{part.name} {part.exercises}</p>)
 }
-const Total = ({num}) =>{
-  return(<p>Number of exercises {num}</p>)
+const Total = ({parts}) =>{
+  var sum = 0
+  parts.forEach(part => {
+    sum = sum + part.exercises
+  });
+  return(<p>Number of exercises {sum}</p>)
 }
 export default App
