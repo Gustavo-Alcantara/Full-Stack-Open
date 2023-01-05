cfed103669c1f8c64f6e3b2449ba134eb818b4df
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2023-01-05 14:31:53 -0300

    The Phonebook Step6 Done

--- Part 2/thephonebook/src/App.js
+++ Part 2/thephonebook/src/App.js
@@ -1,14 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567' 
-    }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+
+  useEffect(() => {
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
+
 
   const handleNameChange = (event) => { setNewName(event.target.value) }
   const handleNumberChange = (event) =>{ setNewNumber(event.target.value) }
--- Part 2/thephonebook/src/index.js
+++ Part 2/thephonebook/src/index.js
@@ -2,5 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
+import axios from 'axios'
+
+const promise = axios.get('http://localhost:3001/persons')
+promise.then(response => {
+    const persons = response.data
+    console.log(persons)
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
