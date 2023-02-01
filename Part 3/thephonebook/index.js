907025a33d8d23340cf911274a6f9ca41a9b222d
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2023-01-31 21:41:03 -0300

    Phonebook backend step 4 done

--- Part 3/thephonebook/index.js
+++ Part 3/thephonebook/index.js
@@ -49,6 +49,12 @@ app.get('/api/persons/:id', (request, response) => {
       response.status(404).end()
     }
 })
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.filter(person => person.id != id)
+  
+  response.status(204).end()
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
